dfd7dedec4b3bb303c5539487ff27cbda324a135
Shun Git <kwokshungit@gmail.com>
2016-03-31 22:13:01 +0800

    add smBrain

---
+++ smBrain.py
@@ -0,0 +1,29 @@
+import sys, os
+sys.path.append( os.getcwd() )
+
+from soar.io import io
+from libdw import gfx
+from robot import RobotMover
+
+
+#####################
+##  Brain methods  ##
+#####################
+
+def setup():
+	robot.gfx = gfx.RobotGraphics()
+	robot.behavior = mover
+
+def brainStart():
+	robot.behavior.start(traceTasks=robot.gfx.tasks())
+
+def step():
+	inp = io.SensorInput()
+	robot.behavior.step(inp).execute()
+	io.done(robot.behavior.isDone())
+
+def brainStop():
+	pass
+
+def shutdown():
+	pass
