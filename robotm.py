04289ef57f3c177651d86bfe04a4af57e2a65122
Shun Git <kwokshungit@gmail.com>
2016-04-01 15:00:15 +0800

    tabs to spaces

--- robotm.py
+++ robotm.py
@@ -4,158 +4,158 @@ from soar.io import io
 
 class RobotMover(sm.SM):
 
-	botwidth = 0.08 # 0.12
-	corridor = 1.5 - botwidth
-	
-	def __init__( self, name, starting='' ):
-		self.startState = starting
-	
-	def done( self, state ):
-		return state[0] == 'H'
+    botwidth = 0.08 # 0.12
+    corridor = 1.5 - botwidth
+    
+    def __init__( self, name, starting='' ):
+        self.startState = starting
 
-	def getNextValues( self, state, inp ):
-		
-		a = io.Action()
+    def done( self, state ):
+        return state[0] == 'H'
 
-		if isinstance( state, basestring ):
-			if state == 'H': return state, a
-			curS, path = 'I', state
-		else:
-			curS, path, jtype = state
-			J = ( ( 'J', path, jtype ), a )
+    def getNextValues( self, state, inp ):
 
-		try:
-			if time.time() - state[0] >= 0:
-				return J
-			else:
-				return state, a
-		except TypeError:
-			pass
+        a = io.Action()
 
-		dist = [ min(2,i) for i in inp.sonars[:5] ]
+        if isinstance( state, basestring ):
+            if state == 'H': return state, a
+            curS, path = 'I', state
+        else:
+            curS, path, jtype = state
+            J = ( ( 'J', path, jtype ), a )
 
-		if curS in 'IJ':
-			if not path: return 'H', a
+        try:
+            if time.time() - state[0] >= 0:
+                return J
+            else:
+                return state, a
+        except TypeError:
+            pass
 
-			if curS == 'I':
-				pass # Init sensors
+        dist = [ min(2,i) for i in inp.sonars[:5] ]
 
-			curS = path[0]
-			if curS == 'S':
-				print 'Waiting, temp: ', inp.temperature
-				curS = int(time.time()) + 8
+        if curS in 'IJ':
+            if not path: return 'H', a
 
-			jtype = sum( 1<<i for i in (0,1,2) if dist[3-i] < 2.0 )
-			return ( ( curS, path[1:], jtype ), a )
+            if curS == 'I':
+                pass # Init sensors
 
-		# print ( "%2s" % curS )
+            curS = path[0]
+            if curS == 'S':
+                print 'Waiting, temp: ', inp.temperature
+                curS = int(time.time()) + 8
 
-		em = ( dist[0] > self.corridor, dist[4] > self.corridor )
-		err = ( dist[0] - dist[4], dist[1] - dist[3] )
+            jtype = sum( 1<<i for i in (0,1,2) if dist[3-i] < 2.0 )
+            return ( ( curS, path[1:], jtype ), a )
 
-		if curS == 'F':
+        # print ( "%2s" % curS )
 
-			szerr = dist[0] + dist[4] - self.corridor
+        em = ( dist[0] > self.corridor, dist[4] > self.corridor )
+        err = ( dist[0] - dist[4], dist[1] - dist[3] )
 
-			#if jtype == 5 and any(em):
-			#	return J
-			#if not ( jtype or any(em) ):
-			#	return J
-			#if jtype == 4 and ( em[0] or not em[1] ):
-			#	return J
-			#if jtype == 1 and ( em[1] or not em[0] ):
-			#	return J
+        if curS == 'F':
 
-			if dist[2] <= 0.8:
-				return J
-			#elif dist[2] < 1.8:
-			#	a.fvel = 0.3
+            szerr = dist[0] + dist[4] - self.corridor
 
-			# any( jtype&(1<<i) and dist[3-i] > 1.5 for i in (0,2) )
-			# if jtype&4 and abs(sur[0]) > 0.5:
-			#	fv = 0.3
-			#elif jtype&1 and abs(sur[1]) > 0.5:
-			#	fv = 0.3
-			if False:
-				pass
-			else:
-				a.fvel = 0.1
-				print '\t'.join( str(round(i,3)) for i in dist[:5] ),
-				
-				if abs(szerr) >= 0.02 or abs(err[0]) >= 0.08:
-					print "\t%.2f\t%.4f" % ( szerr, err[0] )
-					
-					szk = 4.0 if err[0] >= 0 else -4.0
-					
-					a.fvel = 0.05
-					a.rvel = 0.2 * err[0] * ( 1 - szerr )
-				else:
-					print
+            #if jtype == 5 and any(em):
+            #   return J
+            #if not ( jtype or any(em) ):
+            #   return J
+            #if jtype == 4 and ( em[0] or not em[1] ):
+            #   return J
+            #if jtype == 1 and ( em[1] or not em[0] ):
+            #   return J
 
-		elif curS.startswith('R'):
-			a = io.Action(fvel=0.25,rvel=-0.3)
-			
-			if jtype:
-				if em[0]: curS = 'R2' if jtype&2 else 'R1'
-			else:
-				if curS == 'R' and not em[0]:
-					curS = 'R0'
-				elif curS == 'R0' and em[0]:
-					curS = 'R1'
+            if dist[2] <= 0.8:
+                return J
+            #elif dist[2] < 1.8:
+            #   a.fvel = 0.3
 
-			if curS == 'R1' and not em[0]:
-				return J
-			
-			if curS == 'R2' and not any(em) and abs(sum(err)) < 0.1:
-				return J
+            # any( jtype&(1<<i) and dist[3-i] > 1.5 for i in (0,2) )
+            # if jtype&4 and abs(sur[0]) > 0.5:
+            #   fv = 0.3
+            #elif jtype&1 and abs(sur[1]) > 0.5:
+            #   fv = 0.3
+            if False:
+                pass
+            else:
+                a.fvel = 0.1
+                print '\t'.join( str(round(i,3)) for i in dist[:5] ),
 
-		elif curS.startswith('L'):
-			a = io.Action(fvel=0.25,rvel=0.3)
-			
-			if jtype:
-				if em[1]: curS = 'L2' if jtype&2 else 'L1'
-			else:
-				if curS == 'L' and not em[1]:
-					curS = 'L0'
-				elif curS == 'L0' and em[1]:
-					curS = 'L1'
+                if abs(szerr) >= 0.02 or abs(err[0]) >= 0.08:
+                    print "\t%.2f\t%.4f" % ( szerr, err[0] )
 
-			if curS == 'L1' and not em[1]:
-				return J
-			
-			if curS == 'L2' and not any(em) and abs(sum(err)) < 0.1:
-				return J
+                    szk = 4.0 if err[0] >= 0 else -4.0
 
-		elif curS.startswith('U'):
-			a.rvel = 0.3
+                    a.fvel = 0.05
+                    a.rvel = 0.2 * err[0] * ( 1 - szerr )
+                else:
+                    print
 
-			if curS == 'U' and any(em):
-				curS = 'U0'
-			elif curS == 'U0' and not any(em) and abs(sum(err)) < 0.1:
-				return J
+        elif curS.startswith('R'):
+            a = io.Action(fvel=0.25,rvel=-0.3)
 
-		else:
-			print 'What'
-			
-		return ( ( curS, path, jtype ), a )
+            if jtype:
+                if em[0]: curS = 'R2' if jtype&2 else 'R1'
+            else:
+                if curS == 'R' and not em[0]:
+                    curS = 'R0'
+                elif curS == 'R0' and em[0]:
+                    curS = 'R1'
+
+            if curS == 'R1' and not em[0]:
+                return J
+
+            if curS == 'R2' and not any(em) and abs(sum(err)) < 0.1:
+                return J
+
+        elif curS.startswith('L'):
+            a = io.Action(fvel=0.25,rvel=0.3)
+
+            if jtype:
+                if em[1]: curS = 'L2' if jtype&2 else 'L1'
+            else:
+                if curS == 'L' and not em[1]:
+                    curS = 'L0'
+                elif curS == 'L0' and em[1]:
+                    curS = 'L1'
+
+            if curS == 'L1' and not em[1]:
+                return J
+
+            if curS == 'L2' and not any(em) and abs(sum(err)) < 0.1:
+                return J
+
+        elif curS.startswith('U'):
+            a.rvel = 0.3
+
+            if curS == 'U' and any(em):
+                curS = 'U0'
+            elif curS == 'U0' and not any(em) and abs(sum(err)) < 0.1:
+                return J
+
+        else:
+            print 'What'
+
+        return ( ( curS, path, jtype ), a )
 
 if __name__ == "__builtin__":
 
-	path = 'F'
-	# path = 'FLFFFSU'
+    path = 'F'
+    # path = 'FLFFFSU'
 
-	def setup():
-		robot.behavior = RobotMover( 'brainSM', path )
+    def setup():
+        robot.behavior = RobotMover( 'brainSM', path )
 
-	def brainStart():
-		robot.behavior.start()
+    def brainStart():
+        robot.behavior.start()
 
-	def step():
-		robot.behavior.step(io.SensorInput()).execute()
-		io.done(robot.behavior.isDone())
+    def step():
+        robot.behavior.step(io.SensorInput()).execute()
+        io.done(robot.behavior.isDone())
 
-	def brainStop():
-		pass
+    def brainStop():
+        pass
 
-	def shutdown():
-		pass
+    def shutdown():
+        pass
--- smBrain.py
+++ smBrain.py
@@ -12,19 +12,19 @@ mover = RobotMover('bdSM','FFF')
 #####################
 
 def setup():
-	robot.gfx = gfx.RobotGraphics()
-	robot.behavior = mover
+    robot.gfx = gfx.RobotGraphics()
+    robot.behavior = mover
 
 def brainStart():
-	robot.behavior.start(traceTasks=robot.gfx.tasks())
+    robot.behavior.start(traceTasks=robot.gfx.tasks())
 
 def step():
-	inp = io.SensorInput()
-	robot.behavior.step(inp).execute()
-	io.done(robot.behavior.isDone())
+    inp = io.SensorInput()
+    robot.behavior.step(inp).execute()
+    io.done(robot.behavior.isDone())
 
 def brainStop():
-	pass
+    pass
 
 def shutdown():
-	pass
+    pass
