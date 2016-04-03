e71d2ccc25cbffc51a1f490bda63404e3560b32e
Shun Git <kwokshungit@gmail.com>
2016-04-03 10:49:43 +0800

    change path notation and sonar label

--- robotm.py
+++ robotm.py
@@ -8,6 +8,7 @@ class RobotMover(sm.SM):
     corridor = 1.5 - botwidth
     
     def __init__( self, name, starting='' ):
+        self.name = name
         self.startState = starting
 
     def done( self, state ):
@@ -15,134 +16,132 @@ class RobotMover(sm.SM):
 
     def getNextValues( self, state, inp ):
 
+        def dge( d ):
+            if d == 2.5: return 0
+            e = d - 1.02
+            if abs( e ) <= 0.01: return 0
+            return e
+
         a = io.Action()
 
+        if state == 'H':
+            return state, a
+
         if isinstance( state, basestring ):
-            if state == 'H': return state, a
-            curS, path = 'I', state
+            curS, path = 'A', state
         else:
-            curS, path, jtype = state
-            J = ( ( 'J', path, jtype ), a )
+            curS, path = state
 
         try:
-            if time.time() - state[0] >= 0:
-                return J
-            else:
-                return state, a
-        except TypeError:
+            if time.time() - curS >= 0:
+                state = ( 'J', path )
+            return state, a
+        except ( TypeError, IndexError ):
             pass
 
-        dist = [ min(2,i) for i in inp.sonars[:5] ]
-
-        if curS in 'IJ':
-            if not path: return 'H', a
-
-            if curS == 'I':
-                pass # Init sensors
+        dist = [ min( 2.5, inp.sonars[i] ) for i in (2,3,4,0,1) ]
+        
+        if curS == 'J': # Junction
+            if not path:
+                return 'H', a
 
             curS = path[0]
             if curS == 'S':
                 print 'Waiting, temp: ', inp.temperature
                 curS = int(time.time()) + 8
-
-            jtype = sum( 1<<i for i in (0,1,2) if dist[3-i] < 2.0 )
-            return ( ( curS, path[1:], jtype ), a )
-
-        # print ( "%2s" % curS )
-
-        em = ( dist[0] > self.corridor, dist[4] > self.corridor )
-        err = ( dist[0] - dist[4], dist[1] - dist[3] )
-
-        if curS == 'F':
-
-            szerr = dist[0] + dist[4] - self.corridor
-
-            #if jtype == 5 and any(em):
-            #   return J
-            #if not ( jtype or any(em) ):
-            #   return J
-            #if jtype == 4 and ( em[0] or not em[1] ):
-            #   return J
-            #if jtype == 1 and ( em[1] or not em[0] ):
-            #   return J
-
-            if dist[2] <= 0.8:
-                return J
-            #elif dist[2] < 1.8:
-            #   a.fvel = 0.3
-
-            # any( jtype&(1<<i) and dist[3-i] > 1.5 for i in (0,2) )
-            # if jtype&4 and abs(sur[0]) > 0.5:
-            #   fv = 0.3
-            #elif jtype&1 and abs(sur[1]) > 0.5:
-            #   fv = 0.3
-            if False:
-                pass
             else:
+                curS += str( sum( 1<<i for i in (0,1,2) if dist[i*2-2] < self.corridor ) )
+
+            return ( curS, path[1:] ), a
+
+        em = ( dist[0] < self.corridor, dist[2] > self.corridor, dist[-2] > self.corridor )
+
+        lrerr = dist[-2] - dist[2]
+        if abs( lrerr ) <= 0.08: lrerr = 0
+
+        dgerr = ( 0, dge( dist[1] ), dge( dist[-1] ) )
+        
+        if curS == 'A': # Alley
+
+            if any(em):
+                return ( 'J', path ), a
+
+            szerr = dist[-2] + dist[2] - self.corridor
+
+            if szerr <= -0.01:
+                return ( 'O', path ), a
+
+            a.fvel = 0.5
+
+            # print '\t'.join( str(round(i,3)) for i in dist )
+
+            outs = "%.3f\t%.3f\t%.3f\t%.3f" % ( ( szerr, lrerr ) + dgerr[1:] )
+
+            if lrerr:
+                print "lr", outs
+                kz = 0.4 if szerr <= 0.02 else -0.3
                 a.fvel = 0.1
-                print '\t'.join( str(round(i,3)) for i in dist[:5] ),
+                a.rvel = kz*lrerr
 
-                if abs(szerr) >= 0.02 or abs(err[0]) >= 0.08:
-                    print "\t%.2f\t%.4f" % ( szerr, err[0] )
+            elif szerr >= 0.01:
+                print "sz", outs
+                kz = 0
+                if dgerr[-1] < 0:
+                    kz = -2
+                if dgerr[1] < 0:
+                    kz = 2
+                a.fvel = 0.1
+                a.rvel = kz*szerr
+            
+            return ( curS, path ), a
 
-                    szk = 4.0 if err[0] >= 0 else -4.0
+        elif curS.startswith('O'):
+            print "obstacle"
+            return ( curS, path ), a
 
-                    a.fvel = 0.05
-                    a.rvel = 0.2 * err[0] * ( 1 - szerr )
-                else:
-                    print
+        dir = curS[0]
+        jtype = int(curS[1])
+        stage = curS[2:]
 
-        elif curS.startswith('R'):
-            a = io.Action(fvel=0.25,rvel=-0.3)
+        A = ( 'A', path ), a
 
-            if jtype:
-                if em[0]: curS = 'R2' if jtype&2 else 'R1'
-            else:
-                if curS == 'R' and not em[0]:
-                    curS = 'R0'
-                elif curS == 'R0' and em[0]:
-                    curS = 'R1'
+        if dir == 'F':
+            if not any(em[1:]):
+                return A
+            a.fvel = 0.5
 
-            if curS == 'R1' and not em[0]:
-                return J
-
-            if curS == 'R2' and not any(em) and abs(sum(err)) < 0.1:
-                return J
-
-        elif curS.startswith('L'):
-            a = io.Action(fvel=0.25,rvel=0.3)
-
-            if jtype:
-                if em[1]: curS = 'L2' if jtype&2 else 'L1'
-            else:
-                if curS == 'L' and not em[1]:
-                    curS = 'L0'
-                elif curS == 'L0' and em[1]:
-                    curS = 'L1'
-
-            if curS == 'L1' and not em[1]:
-                return J
-
-            if curS == 'L2' and not any(em) and abs(sum(err)) < 0.1:
-                return J
-
-        elif curS.startswith('U'):
+        elif dir == 'U':
             a.rvel = 0.3
 
-            if curS == 'U' and any(em):
-                curS = 'U0'
-            elif curS == 'U0' and not any(em) and abs(sum(err)) < 0.1:
-                return J
+            if not stage and any(em[1:]):
+                stage = 'A'
+            elif stage == 'A' and not any(em[1:]) and abs( lrerr ) < 0.1:
+                return A
 
         else:
-            print 'What'
+            if stage == 'C' and not any(em[1:]):
+                return A
 
-        return ( ( curS, path, jtype ), a )
+            z = 1 if dir == 'L' else -1
+
+            if stage == 'B' and not em[z]:
+                return A
+
+            a = io.Action(fvel=0.25,rvel=z*0.3)
+
+            if jtype:
+                if em[z]: stage = 'C' if jtype&2 else 'B'
+            else:
+                if not stage and not em[z]:
+                    stage = 'A'
+                elif stage == 'A' and em[z]:
+                    stage = 'B'
+
+        return ( "%s%d%s" % ( dir, jtype, stage ), path ), a
 
 if __name__ == "__builtin__":
 
     path = 'F'
-    # path = 'FLFFFSU'
 
     def setup():
         robot.behavior = RobotMover( 'brainSM', path )
